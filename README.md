8b81e77a7fb68e9f7069e176c8063896573ce362
Justin Beckwith <justin.beckwith@gmail.com>
2020-12-19 21:21:47 -0800

    👋🌍

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # linkinator-action
 A GitHub Action that checks your README and other markdown for 404s.
+
+## Inputs
+- `paths` - Paths to scan for 404s. Defaults to `*.md`.
+- `concurrency` - The number of connections to make simultaneously. Defaults to `100`.
+- `recurse` - Recursively follow links on the same root domain.  Defaults to `false`.
+- `linksToSkip` - List of urls in regexy form to not include in the check.
+- `timeout` - Request timeout in ms.  Defaults to 0 (no timeout).
+- `markdown` - Automatically parse and scan markdown if scanning from a location on disk. Defaults to `true`.
+- `serverRoot` - When scanning a locally directory, customize the location on disk where the server is started.  Defaults to the root of your GitHub repository.
+
+## Outputs
+- `results` - An object with the results of the run.
+
+## Example usage
+
+uses: linkinator-action@v1
+with:
+  paths: '*.md'
+  concurrency: 10
+
---
+++ src/index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core');
+const { LinkChecker } = require('linkinator');
+
+async function main () {
+  try {
+    const options = {
+      path: core.getInput('paths'),
+      concurrency: core.getInput('concurrency'),
+      recurse: core.getInput('recurse'),
+      linksToSkip: core.getInput('skip'),
+      timeout: core.getInput('timeout'),
+      markdown: core.getInput('markdown') ?? true,
+      serverRoot: core.getInput('serverRoot')
+    };
+
+    const checker = new LinkChecker()
+      .on('pagestart', url => {
+        console.log(`Scanning ${url}`);
+      });
+
+    const result = await checker.check(options);
+    if (!result.passed) {
+      const brokenLinks = result.links.filter(x => x.state === 'BROKEN');
+      let failureOutput = `Detected ${brokenLinks.length} broken links.`;
+      for (const link of brokenLinks) {
+        failureOutput += `\n [${link.status}] ${link.url}`;
+      }
+      throw new Error(failureOutput);
+    }
+    console.log(`Scanned total of ${result.links.length} links!`);
+    core.setOutput('results', result);
+  } catch (err) {
+    core.setFailed(`linkinator failed: \n${err.message}`);
+  }
+}
+
+if (require.main === module) {
+  main();
+} else {
+  module.exports = main;
+}
