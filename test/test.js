1ab5a1a8ff1a73752e70cf62a178f71253851f95
Justin Beckwith <beckwith@google.com>
2020-12-19 21:29:11 -0800

    👋🌍 (#1)

---
+++ test/test.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const core = require('@actions/core');
+const { describe, it, afterEach } = require('mocha');
+const sinon = require('sinon');
+const nock = require('nock');
+const action = require('../src/index.js');
+
+nock.disableNetConnect();
+nock.enableNetConnect('localhost');
+
+describe('linkinator action', () => {
+  afterEach(() => {
+    sinon.restore();
+    nock.cleanAll();
+  });
+
+  it('should return ok for a valid README', async () => {
+    const inputStub = sinon.stub(core, 'getInput')
+      .withArgs('paths').returns('test/fixtures/test.md');
+    const setOutputStub = sinon.stub(core, 'setOutput');
+    const setFailedStub = sinon.stub(core, 'setFailed');
+    const scope = nock('http://fake.local')
+      .head('/').reply(200)
+      .head('/fake').reply(200);
+    await action();
+    assert.ok(inputStub.called);
+    assert.ok(setOutputStub.called);
+    assert.ok(setFailedStub.notCalled);
+    scope.done();
+  });
+
+  it('should call setFailed on failures', async () => {
+    const inputStub = sinon.stub(core, 'getInput')
+      .withArgs('paths').returns('test/fixtures/test.md');
+    const setOutputStub = sinon.stub(core, 'setOutput');
+    const setFailedStub = sinon.stub(core, 'setFailed');
+    const scope = nock('http://fake.local')
+      .head('/').reply(404)
+      .head('/fake').reply(404);
+    await action();
+    assert.ok(inputStub.called);
+    assert.ok(setOutputStub.notCalled);
+    assert.ok(setFailedStub.called);
+    scope.done();
+  });
+});
