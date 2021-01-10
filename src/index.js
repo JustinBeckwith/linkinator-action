258f945c46b82da4f30ec2ade35aa85ecc1ab2aa
Justin Beckwith <justin.beckwith@gmail.com>
2021-01-09 23:19:52 -0800

    mhm

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ async function main () {
       timeout: Number(qq('timeout', 0)),
       markdown: Boolean(qq('markdown', true)),
       serverRoot: qq('serverRoot', undefined),
-      directoryListing: Boolean(qq('directoryListing', false)),
+      directoryListing: Boolean(qq('directoryListing', false))
     };
 
     const checker = new LinkChecker()
